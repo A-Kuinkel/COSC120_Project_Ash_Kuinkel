f767c27a35d69bd463be61c3bc5e339a4e3f7b66
A-Kuinkel <akuinke3@myune.edu.au>
2026-07-20 21:45:20 +1000

    feat: initialise enums

--- src/enums/Brand.java
+++ src/enums/Brand.java
@@ -1,4 +1,29 @@
 package enums;
 
-public enum Brands {
+public enum Brand {
+    // our list of brands:
+    AMD, ASUS, APPLE, COOLERMASTER, DELL, HP, INTEL, LENOVO, LG, LOGITECH, MSI, NVIDIA, SAMSUNG;
+
+    /**
+     * This is a toString() method, so that the brand enums can be expressed in a more aesthetically pleasing way.
+     * @return just a nicer string version of the brand name e.g. 'Cooler Master' instead of 'COOLERMASTER'.
+     */
+    @Override
+    public String toString(){
+        return switch (this){
+            case AMD -> "AMD";
+            case ASUS -> "ASUS";
+            case APPLE -> "Apple";
+            case COOLERMASTER -> "Cooler Master";
+            case DELL -> "Dell";
+            case HP -> "HP";
+            case INTEL -> "Intel";
+            case LENOVO -> "Lenovo";
+            case LG -> "LG";
+            case LOGITECH -> "Logitech";
+            case MSI -> "MSI";
+            case NVIDIA -> "NVIDIA";
+            case SAMSUNG -> "Samsung";
+        };
+    }
 }
--- src/enums/Category.java
+++ src/enums/Category.java
@@ -2,4 +2,26 @@ package enums;
 
 public enum Category {
     // categories, i.e. laptop, mouse, keyboards etc
+    LAPTOPS, MOUSE, KEYBOARD, MONITOR, DESKTOP, GPU, CPU, RAM, PSU, CASE, COOLING;
+
+    /**
+     * This is a toString() method, implemented so that the category enums can be expressed in a more pretty way.
+     * @return just a nicer string version of the category name e.g. 'Prebuilt Desktops' instead of just 'DESKTOP'.
+     */
+    @Override
+    public String toString(){
+        return switch (this) {
+            case MOUSE -> "Mouse";
+            case LAPTOPS -> "Laptops";
+            case KEYBOARD -> "Keyboards";
+            case MONITOR -> "Monitors";
+            case DESKTOP -> "Prebuilt Desktops";
+            case GPU -> "GPU";
+            case CPU -> "CPU";
+            case RAM -> "RAM";
+            case PSU -> "PSU";
+            case CASE -> "PC Cases";
+            case COOLING -> "Cooling";
+        };
+    }
 }
