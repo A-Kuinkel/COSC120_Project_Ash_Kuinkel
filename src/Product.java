a6a8ef252bfe79b46da03acbe8fe00f9445f970d
A-Kuinkel <akuinke3@myune.edu.au>
2026-07-20 21:46:02 +1000

    feat: initialise Product fields

--- src/Product.java
+++ src/Product.java
@@ -1,6 +1,28 @@
+import enums.Brand;
+import enums.Category;
+
 public class Product {
 
     // fields:
+    final private int id;
+    private String name;
+    private float price;
+    private String description;
+    private int quantity;
+    Category category;
+    Brand brand;
+
+    // constructor
+    Product(int id, String name, float Price, String description,
+            int quantity, Category category, Brand brand){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.quantity = quantity;
+        this.category = category;
+        this.brand = brand;
+    }
 
     // getters/setters:
 
