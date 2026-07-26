8dd220a4757de115e1e66f14fbc03c06a6569643
A-Kuinkel <akuinke3@myune.edu.au>
2026-07-26 21:54:40 +1000

    Initialise User fields

--- src/User.java
+++ src/User.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class User {
+
+    private String firstName;
+    private String lastName;
+    private String contactEmail;
+    private int contactPhoneNumber;
+    private String deliveryAddress;
 }
