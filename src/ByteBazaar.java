00e1832c6f138d28f6eadcd13ac0aff48ccd2663
A-Kuinkel <akuinke3@myune.edu.au>
2026-07-27 23:19:07 +1000

    feat: product page initialisations

--- src/ByteBazaar.java
+++ src/ByteBazaar.java
@@ -1,6 +1,16 @@
 import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ByteBazaar {
+
+    private static final String productsFilePath = "src/allProducts.txt";
+
 
     public static void main(String[] args) {
 
@@ -34,6 +44,11 @@ public class ByteBazaar {
                     case 1:
                         // take to first page
                         System.out.println("First page!");
+                        try {
+                            loadProductPage();
+                        } catch (IOException e) {
+                            System.out.println("The page requested could not be loaded. **ERROR**: \n" + e);
+                        }
                         break;
                     case 2:
                         // take to second page
@@ -52,5 +67,37 @@ public class ByteBazaar {
                         " Please enter an integer from the following options provided!");
             }
         } while (userSelectedOption != 1 && userSelectedOption != 2 && userSelectedOption != 3);
+    }
+
+    private static void loadProductPage() throws IOException {
+        System.out.println("Loading ByteBazaar products...");
+        Path productsFile = Path.of(productsFilePath);
+
+        List<String> lines = Files.readAllLines(productsFile);
+        lines.removeFirst();
+
+        for(String line: lines){
+            String[] productInfo = line.split(",");
+
+            String productName = productInfo[1];
+            String productPrice = productInfo[4];
+            float productRating = Float.parseFloat(productInfo[6]);
+            boolean productIsWireless = productInfo[7].equalsIgnoreCase("yes");
+            boolean productOnSale = productInfo[8].equalsIgnoreCase("yes");
+            // some of the warranty values are null... so, ADD VALIDATION CHECK HERE:
+            int productWarrantyYears = productInfo[9].equalsIgnoreCase("N/A") ? Integer.parseInt(productInfo[9]) : -1;
+            String productColour = productInfo[10];
+            String productDescription = productInfo[12];
+            String productDisplayImage = productInfo[13];
+
+
+
+            List<String> filteredProducts = new ArrayList<>();
+            filteredProducts.add(productInfo[0]);
+            filteredProducts.add(productName);
+            filteredProducts.add(productPrice);
+            System.out.println(filteredProducts);
+        }
+
     }
 }
