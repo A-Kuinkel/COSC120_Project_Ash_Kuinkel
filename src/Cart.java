ad716560aab4df2ae75457e6b3d27d7425134c22
A-Kuinkel <akuinke3@myune.edu.au>
2026-09-05 10:49:57 +1000

    start cart jdocs

--- src/Cart.java
+++ src/Cart.java
@@ -8,44 +8,79 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The session-based cart class. The job of this class is to manage the user's cart. When the user logs-in we assign
+ * a new instance of the Cart class to them & then this cart is able to add products, clear etc. Note that since the
+ * assignment says we are only allowed one txt file, we cannot save the users cart after the program is closed.
+ */
 public class Cart {
 
-    // The source code is only meant to have like one txt file... so this means that what I was thinking before
-    // shouldn't really be the way to approach it. Better to just have the cart remember only the current running
-    // session because there isn't really anywhere appropriate we can save the user's cart/order info without creating
-    // another .txt db file.
-
     // fields
-    private final List<Product> products = new ArrayList<>();
-    private final User user;
+    private final List<Product> products = new ArrayList<>(); //
+    private final User user; // declared as final because
 
+    /**
+     *
+     * @param user
+     */
     Cart (User user) {
         this.user = user;
     }
 
     // getters/setters
+
+    /**
+     * Method which
+     * @return
+     */
     public List<Product> getProducts() {return new ArrayList<>(products);}
+
+    /**
+     *
+     * @return
+     */
     public User getUser() {return user;}
 
     // methods
+
+    /**
+     * Method to add a product to the user's cart. Called from the program class.
+     * @param product represents the product that is to be added to the cart.
+     */
     public void addProductToCart(Product product) {
         products.add(product);
     }
 
+    /**
+     * Method to clear the user's cart {uses the convenient .clear() method for ArrayLists in Java}. Program
+     * features clear cart method only rather than a method to remove a singular product from cart for simplicity.
+     */
     public void clearCart() {
         products.clear();
     }
 
+    /**
+     * Method to calculate the total price of all the products within the cart.
+     * @return the total price of all the items which the user is required to pay.
+     */
     public double getTotalPrice() {
         double total = 0;
         for (Product product : products) total += product.getPrice();
         return total;
     }
 
+    /**
+     * Method to check if the user's cart is empty at a certain moment, i.e. no products in cart.
+     * @return true if the Cart is empty else return false.
+     */
     public boolean isEmpty(){
         return products.isEmpty();
     }
 
+    /**
+     *
+     * @return the well-formatted string containing the user's cart (to be displayed in JOptionPne window).
+     */
     @Override
     public String toString() {
 
